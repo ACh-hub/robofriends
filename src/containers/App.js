c971e8969e95d4c63d512cdf0f0df756355aa38b
Aleksandra Chrapkowska <amchrapkowska@gmail.com>
2019-09-19 11:34:27 +0200

    minor performance fixes

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,6 +6,7 @@ import './App.css'
 import Scroll from '../components/Scroll'
 import ErrorBoundry from '../components/ErrorBoundry'
 import {setSearchField, requestRobots} from '../actions'
+import Header from '../components/Header'
 
 
 const mapStateToProps = state => {
@@ -40,7 +41,7 @@ class App extends Component
         else {
             return (
                 <div className='tc'>
-                    <h1 className='title'>RoboFriends</h1>
+                    <Header />
                     <SearchBox searchChange={onSearchChange}/>
                     <Scroll>
                         <ErrorBoundry>
